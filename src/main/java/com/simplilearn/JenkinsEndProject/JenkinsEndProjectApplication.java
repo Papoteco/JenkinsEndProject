9fc5de19ebb03832588346e76b3f86c4a1da24f0
student <rafacanelon@gmail.com>
2021-02-13 21:45:02 +0000

    adding the Maven app created via spring boot

---
+++ src/main/java/com/simplilearn/JenkinsEndProject/JenkinsEndProjectApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.JenkinsEndProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsEndProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsEndProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/JenkinsEndProject/JenkinsEndProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.JenkinsEndProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsEndProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
